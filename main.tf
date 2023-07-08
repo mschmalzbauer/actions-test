dc693f005b238850c9d86f82646e9cabfa465a16
mschmalzbauer <110097288+mschmalzbauer@users.noreply.github.com>
2023-07-08 17:55:17 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 1.0.4"
+      source  = "hashicorp/azurerm"
+      version = "=3.64.0"
     }
   }
 }
